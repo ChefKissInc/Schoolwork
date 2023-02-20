af7fa8baf8fa89ac8908216aaba2aedda4952f18
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-02-20 09:35:41 +0200

    Add student view and attendance management
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -12,7 +12,9 @@ def get_db() -> sqlite3.Connection:
         cur = db.cursor()
         cur.execute(
             "CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY AUTOINCREMENT, first_name NOT NULL, last_name NOT NULL, class STRING)")
+        cur.execute("CREATE TABLE IF NOT EXISTS apousies(id INTEGER NOT NULL, hour INTEGER NOT NULL, day INTEGER NOT NULL, month INTEGER NOT NULL, year INTEGER NOT NULL)")
         cur.close()
+        db.commit()
     return db
 
 
@@ -36,28 +38,54 @@ def convert() -> str:
     return render_template("convert.html", result=result)
 
 
-@app.route("/<int:wow>")
-def hack_free_works(wow) -> str:
-    return f"Hack NASA free works 2022: {wow + 5}"
+@app.route("/students", methods=['GET', 'POST'])
+@app.route("/students/<int:sid>", methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def students(sid: int = None) -> str:
+    db = get_db()
+    cur = db.cursor()
+    if request.method == 'GET' and not sid:
+        students = cur.execute("SELECT * FROM students").fetchall()
 
-
-@app.route("/studentManagement", methods=['GET', 'POST', 'PATCH', 'DELETE'])
-def student_mgmt() -> str:
-    cur = get_db().cursor()
-    if request.method == 'POST':
+        def fixup(v):
+            v["apousies"] = len(
+                cur.execute("SELECT * FROM apousies WHERE id = ?", (v["id"],)).fetchall())
+            return v
+        students = [fixup(dict(v)) for v in students]
+        cur.close()
+        return render_template("students.html", students=students)
+    elif request.method == 'GET':
+        student = dict(cur.execute(
+            "SELECT * FROM students WHERE id = ?", (sid,)).fetchone())
+        apousies = cur.execute(
+            "SELECT * FROM apousies WHERE id = ?", (sid,)).fetchall()
+        student["apousies"] = apousies
+        cur.close()
+        return render_template("view_student.html", student=student)
+    elif request.method == 'POST':
         data = request.get_json()
-        cur.execute("INSERT INTO students (first_name, last_name, class) VALUES (?, ?, ?)",
-                    (data["first_name"], data["last_name"], data.get("class", None)))
+        apousia = data.get("apousia")
+        if not apousia:
+            cur.execute("INSERT INTO students (first_name, last_name, class) VALUES (?, ?, ?)",
+                        (data["first_name"], data["last_name"], data.get("class", None)))
+        else:
+            cur.execute("INSERT INTO apousies (id, hour, day, month, year) VALUES (?, ?, ?, ?, ?)",
+                        (sid, apousia["hour"], apousia["day"], apousia["month"], apousia["year"]))
     elif request.method == 'PATCH':
         data = request.get_json()
-        cur.execute("UPDATE students SET first_name = ?, last_name = ?, class = ? WHERE id = ?",
-                    (data["first_name"], data["last_name"], data.get("class", None), data["id"]))
+        apousia = data.get("apousia")
+        if not apousia:
+            cur.execute("UPDATE students SET first_name = ?, last_name = ?, class = ? WHERE id = ?",
+                        (data["first_name"], data["last_name"], data.get("class", None), sid))
+        else:
+            cur.execute("UPDATE apousies SET hour = ?, day = ?, month = ?, year = ? WHERE id = ? AND hour = ? AND day = ? AND month = ? AND year = ?",
+                        (data["hour"], data["day"], data["month"], data["year"], sid, apousia["hour"], apousia["day"], apousia["month"], apousia["year"]))
     elif request.method == 'DELETE':
         data = request.get_json()
-        cur.execute("DELETE FROM students WHERE id = ?", (data["id"],))
-    else:
-        students = cur.execute("SELECT * FROM students").fetchall()
-        cur.close()
-        return render_template("student_mgmt.html", students=students)
+        if not data:
+            cur.execute("DELETE FROM students WHERE id = ?", (sid,))
+        else:
+            cur.execute("DELETE FROM apousies WHERE id = ? AND hour = ? AND day = ? AND month = ? AND year = ?",
+                        (sid, data["hour"], data["day"], data["month"], data["year"]))
     cur.close()
+    db.commit()
     return ''
